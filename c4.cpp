012c1b8e06afd82fe0daa98fdcf8032a8d1b4482
Ayush <singhayushayush1@gmail.com>
2026-09-16 23:22:54 +0530

    Initial commit with c4.cpp only

---
+++ c4.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main() {
+
+    // I use boolians //
+
+    bool isOnline = true;
+
+    if(isOnline){
+        printf("you are online");
+    }
+    else{
+        printf("you are offline");
+    }
+
+    return 0;
+
+}
+    
